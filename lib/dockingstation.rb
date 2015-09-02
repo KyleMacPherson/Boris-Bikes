30a8884361c31539f69087b274495dbe07591445
kyle macpherson <kyle1@kyles-MacBook-Pro.local>
2015-09-02 17:24:45 +0100

    post challenge 1 completed up to athletic waypoint

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -3,7 +3,12 @@ require_relative "bike.rb"
 class DockingStation
 
   def release_bike
-    Bike.new
+    fail 'No bikes available' unless @bike
+    @bike
+  end
+
+  def dock bike
+    @bike = bike
   end
 
 end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -11,11 +11,21 @@ describe DockingStation do
     context 'user wants a bike to be released' do
 
       it 'releases a working bike' do
+        subject.dock Bike.new
         expect(subject.release_bike.working?).to be_truthy
       end
 
     end
 
+    context 'responds to dock method' do
+      it {is_expected.to respond_to(:dock).with(1).argument}
+    end
+
+    context 'raises an error when no bikes are available' do
+      it 'displays error message' do
+        expect {subject.release_bike}.to raise_error 'No bikes available'
+      end
+    end
 
   end
 
