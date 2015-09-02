0ba419e9775414e613c0c10a48b7ab144a6d3486
kyle macpherson <kyle1@kyles-MacBook-Pro.local>
2015-09-02 16:32:22 +0100

    challenges 1-10 completed

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -7,6 +7,17 @@ describe DockingStation do
     context 'responds to release_bike method' do
       it { is_expected.to respond_to :release_bike}
     end
+
+    context 'user wants a bike to be released' do
+
+      it 'releases a working bike' do
+        expect(subject.release_bike.working?).to be_truthy
+      end
+
+    end
+
+
   end
+
 
 end
